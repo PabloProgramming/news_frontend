6c2dec3e64b3527ff9310ba5845177ce6ea1a15e
PabloProgramming <pmontalvoster@gmail.com>
2025-04-03 18:00:40 +0100

    feat: add error handling class, modify useApiRequest, and implement across components - Introduced `ErrorHandler` class to centralize error management. - Modified `useApiRequest` hook to utilize the new error handling class. - Applied error handling to all relevant components. - Created `NotFoundPage` component to display a custom 404 error page. - Updated routes to include the `/404` path for non-existent resources.

--- src/components/RoutesApp.jsx
+++ src/components/RoutesApp.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router";
 import {Articles} from "./Articles";
 import {Article} from "./Article";
 import {Topics} from "./Topics";
+import { NotFoundPage } from "./NotFoundPage";
 export const RoutesApp = () => {
   return (
     <Routes>
@@ -9,7 +10,10 @@ export const RoutesApp = () => {
       <Route path="/articles/:article_id" element={<Article />} />
       <Route path="/topics" element={<Topics />} />
       <Route path="topics/:slug/articles" element={<Articles />} />
+      <Route path="/404" element={<NotFoundPage />} />
     </Routes>
   );
 };
+
+
 
