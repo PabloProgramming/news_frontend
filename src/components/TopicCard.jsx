3e3a2cf0813d4206698eb3038079da12d9183e77
PabloProgramming <pmontalvoster@gmail.com>
2025-04-04 15:59:06 +0100

    add default topic photo

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -4,8 +4,10 @@ export const TopicCard = ({topic}) => {
   return (
     <li>
       <Link to={`/topics/${topic.slug}/articles`}>
-        {topic.img_url && (
+        {topic.img_url ? (
           <img src={topic.img_url} alt={`image of ${topic.slug}`} />
+        ) : (
+          <img src="../../public/topic_feature.jpg" alt="topic default image" />
         )}
         <h2>{topic.slug}</h2>
         <p className="topic-description">{topic.description}</p>
